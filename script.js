36f5bb3de5c296de864484a387fbf696fba364eb
Finn Stitcher <foundfootage@protonmail.com>
2022-05-17 23:45:00 -0700

    password parameter selection

--- script.js
+++ script.js
@@ -1,6 +1,45 @@
 /* ASSIGNMENT CODE START */
 
 function generatePassword() {
+    var characterString = "";
+    var passwordLength = window.prompt("Enter a number between 8 and 128 to be the length of your password.");
+    passwordLength = parseInt(passwordLength);
+    
+    // make sure a valid number was input
+    if (isNaN(passwordLength)) {
+        window.alert("Please enter a number.");
+    }
+    else if (passwordLength < 8 || passwordLength > 128) {
+        window.alert("Please enter a number between 8 and 128.");
+    }
+    else {
+        // generate character set
+        var useLowercase = window.confirm("Include lowercase letters?");
+        var useUppercase = window.confirm("Include uppercase letters?");
+        var useNumbers = window.confirm("Use numbers?");
+        var useSpecial = window.confirm("Use special characters?");
+
+        if (useLowercase) {
+             characterString += "abcdefghijklmnopqrstuvwxyz";
+        };
+        if (useUppercase) {
+            characterString += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        };
+        if (useNumbers) {
+            characterString += "1234567890";
+        };
+        if (useSpecial) {
+            characterString += "!\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'";
+            console.log(characterString);
+        };
+
+        // make sure they didn't click no to everything
+        if (characterString === "") {
+            window.alert("You disabled all character types, so a password can't be generated. Try again.");
+        };
+
+        // PUT RANDOM NUMBER GENERATOR HERE
+    }
 };
 
 /* ASSIGNMENT CODE END */
